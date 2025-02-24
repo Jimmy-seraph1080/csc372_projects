78da96e0f51641a284b9b16ff7d5736e221e3efa
Jimmy <zhangjimmy73@gmail.com>
2025-02-23 19:23:59 -0500

    update 11

---
+++ JavaScript/jquery.js
@@ -0,0 +1,25 @@
+// change title text color and increase the front size to 60px
+$(".title").css({"color": "#00ff00", "font-size": "60px"});
+// add a green boarder to each step card
+$("#steps .card").each(function() {
+    $(this).css("border", "2px solid #00ff00")
+});
+// push a new FAQ item to the FAQ Section
+$("#faq .list-group").append('<a href="https://www.reddit.com/r/buildapc/comments/s947ea/how_to_take_care_of_your_pc/" class="list-group-item list-group-item-action bg-dark text-white border-light">What is the best way to maintain my PC?</a>');
+
+// insert a note warning the user
+$("#steps").prepend('<section id="dynamicSection" class="container content mt-4"><h2>Attention!</h2><p>Keep in mind that it’s impossible to write a perfect step-by-step guide to assembling a PC since there are too many variables at play, especially in case design. As such, some steps may vary slightly when building your own PC, but the bulk of the process should follow these steps. While I’ve attempted to write out the steps, many people find it easier to watch a video of their specific PC being assembled, as seeing it done helps a lot. Once you understand you can close this selection by clicking on the button below</p><button id="remove_section" class="btn btn-danger mt-2">Remove Section</button></section>');
+//using $(document).ready(function() ensures the jquery code run only after the whole html are loaded by the browser
+$(document).ready(function() {
+    $("#dynamicSection h2").css({
+        "font-size": "50px",   // Increase font size
+        "font-weight": "bold", // Make it bold
+        "color": "red"         // Make it stand out in red
+    });
+});
+// Remove the dynamicSection by added remove section on button click
+$(document).on("click", "#remove_section", function() {
+    $("#dynamicSection").fadeOut(500, function(){
+        $(this).remove();
+    });
+});
--- JavaScript/navbar.js
+++ JavaScript/navbar.js
@@ -1,9 +1,8 @@
 // use hamburger_icon for getElementById ID
-const hamburger_icon = document.getElementById("hamburger_icon");
-// using querySelectorAll for menu items
-const menu_list_items = document.querySelectorAll("#nav_bar li"); 
-const para = document.querySelector(".phrase");
-    para.innerHTML = "<p><u>Your trusted partner in PC building and parts</u></p>" ;
+// const hamburger_icon = document.getElementById("hamburger_icon");
+// // using querySelectorAll for menu items
+// const menu_list_items = document.querySelectorAll("#nav_bar li"); 
+    $(".phrase").html("<p><u>Your trusted partner in PC building and parts</u></p>" );
 // toggle the navigation menu on click
 // to do that we use an addEventListener
 // an addEventListener method is a function that allows one to attach a handler to an element on a webpage
@@ -13,7 +12,9 @@ const para = document.querySelector(".phrase");
 // use an addEventListener with a parameter of click(eventType) and a function that toggles an 
 // active class on the navigation menu (nav_menu)-> control visibility of the nav_menu
 // in addition it let change selects the class phrase and change the context by giving it an underline
-hamburger_icon.addEventListener("click", () => {
-    const nav_menu = document.getElementById("nav_bar");
-    nav_menu.classList.toggle("active");
-});
+// hamburger_icon.addEventListener("click", () => {
+//     const nav_menu = document.getElementById("nav_bar");
+//     nav_menu.classList.toggle("active");
+// });
+ 
+
