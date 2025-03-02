0805c4fd7e5011d81f5f749084054ae729f9c407
Jimmy <zhangjimmy73@gmail.com>
2025-03-01 19:48:21 -0500

    update3

--- JavaScript/load_content.js
+++ JavaScript/load_content.js
@@ -1,27 +1,31 @@
-// Function to load an external HTML file into a specified div using AJAX
-function loadShopContent() {
-    // Create a new XMLHttpRequest object
-    var xhr = new XMLHttpRequest();
-    
-    // Define the function to run when the request state changes
-    xhr.onreadystatechange = function() {
-        // Check if the request is complete (readyState 4) and successful (status 200)
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            // Insert the loaded HTML content into the div with ID "dynamic-content"
-            document.getElementById("dynamic-content").innerHTML = xhr.responseText;
+// Function to set the opacity of all image elements to 0.5
+function setAllImagesOpacity() {
+    // Restricting selection to images inside the #book container
+    const images = document.querySelectorAll("#product img");
+    images.forEach(function(img) {
+        img.style.opacity = "0.5";
+    });
+}
+
+
+// Function to trigger an Ajax request to load HTML content from a given file path
+function loadHTML(filePath) {
+    xhr = new XMLHttpRequest();
+   
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            document.getElementById("content").innerHTML = xhr.responseText;
+           
         }
     };
-    
-    // Prepare the AJAX request (GET request for the shop.html file, asynchronous)
-    xhr.open("GET", "shop-content.html", true);
-    
-    // Send the request to fetch the file
+    xhr.open("GET", filePath, true);
     xhr.send(null);
 }
 
+
 document.addEventListener("DOMContentLoaded", function() {
     // Get the image elements using their IDs
-    const donQuixoteImg = document.getElementById("card-img-top");
+    const donQuixoteImg = document.getElementById("4090");
 
 
     if (donQuixoteImg) {
