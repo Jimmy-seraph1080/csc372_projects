7b1da084efbf376fd9fa538dc4dcb6e1d77dd308
Jimmy <zhangjimmy73@gmail.com>
2025-03-02 16:04:10 -0500

    update16

--- JavaScript/load_content.js
+++ JavaScript/load_content.js
@@ -1,44 +1,32 @@
-// Function to set the opacity of all image elements to 0.5
+// Function to set the opacity of all images inside the #product container
 function setAllImages() {
-    // Restricting selection to images inside the #book container
     const images = document.querySelectorAll("#product img");
-    images.forEach(function(img) {
-        img.style.opacity = "0.5";
-    });
+    images.forEach(img => img.style.opacity = "0.5");
 }
 
-
-// Function to trigger an Ajax request to load HTML content from a given file path
+// Function to trigger an Ajax request to load HTML content
 function loadHTML(filePath) {
-    xhr = new XMLHttpRequest();
+    let xhr = new XMLHttpRequest();
    
     xhr.onload = function() {
         if (xhr.status === 200) {
-            document.getElementById("content").innerHTML = xhr.responseText;
-           
+            document.getElementById("details").innerHTML = xhr.responseText; // Load into "details"
         }
     };
+
     xhr.open("GET", filePath, true);
     xhr.send(null);
 }
 
-
+// Event Listener for loading HTML content when an image is clicked
 document.addEventListener("DOMContentLoaded", function() {
-    // Get the image elements using their IDs
     const gpu4090 = document.getElementById("4090");
-    const i9 = document.getElementById("i9");
 
     if (gpu4090) {
         gpu4090.addEventListener("click", function() {
             setAllImages();
-            loadHTML("data/shop-content.html");
-        });
-    }
-
-    if (i9) {
-        gpu4090.addEventListener("click", function() {
-            setAllImages();
-            loadHTML("data/shop-content.xml");
+            this.style.opacity = "1"; // Highlight selected image
+            loadHTML("data/gpu-content.html"); // Load GPU details
         });
     }
 });
