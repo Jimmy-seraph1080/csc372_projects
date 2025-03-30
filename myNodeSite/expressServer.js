90ada1830112572e3f50ba9b8deec00a7c28bb9c
Jimmy <zhangjimmy73@gmail.com>
2025-03-30 16:01:01 -0400

    update 2

--- myNodeSite/expressServer.js
+++ myNodeSite/expressServer.js
@@ -1,19 +1,22 @@
+// allows us to express modules
 var express = require('express');
-var path = require('path');
+// this is how we'll access all of the functionality of the module
 var app = express();
+// port number
 var port = 1337;
 
-// Set path to public directory
+// create path to public directory
 var dir = __dirname + '/public/';
 
-// Serve static files from directories
+// serve static files from public directories
+// look for static file to be served inside public, public/css, public/images, public/js, public/data directories
 app.use(express.static('public'));
 app.use(express.static('public/css'));
 app.use(express.static('public/images'));
 app.use(express.static('public/js'));
 app.use(express.static('public/data'));
 
-// Route handlers
+// get the respecyive route
 app.get('/', function(request, response) {
     response.sendFile(dir + 'index.html');
 });
@@ -30,12 +33,12 @@ app.get('/shop', function(request, response) {
     response.sendFile(dir + 'shop.html');
 });
 
-// 404 handler - must be last route
+// add  a wildcard for any route that is not defined and respond by serving 404 HTML file
 app.use('/*', function(request, response){
     response.sendFile(dir + '404.html');
 });
 
-// Start server
+// start server
 app.listen(port, function(){
     console.log("Listening... Go to http://localhost:" + port);
 });
