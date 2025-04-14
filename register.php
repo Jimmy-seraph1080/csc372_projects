2ebe09a9cf746ff78561f5688599a6c0929b37d0
Jimmy <zhangjimmy73@gmail.com>
2025-04-14 03:50:40 -0400

    update2

--- register.php
+++ register.php
@@ -106,6 +106,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     <li class="nav-item">
                         <a class="nav-link text-white fw-bold px-lg-5" href="https://jimmyzhang.rhody.dev/csc372_projects/index.html">CONTACT US</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white fw-bold px-lg-5" href="https://jimmyzhang.rhody.dev/csc372_projects/register.php">SIGN UP</a>
+                    </li>       
                 </ul>
             </div>
 
